78e5c482f31568e78bad756af8862311a7451edb
sumin <sumin060302@gmail.com>
2023-02-01 17:44:21 +0900

    2023-02-01 순수 JPA 기반 Repository 생성

--- src/main/java/study/datajpa/DataJpaApplication.java
+++ src/main/java/study/datajpa/DataJpaApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DataJpaApplication {
+public class
+DataJpaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DataJpaApplication.class, args);
